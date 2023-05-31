dba093ad1ac898219fdd6264b40e365adde51548
Pdf4u <131749088+Pdf4u@users.noreply.github.com>
2023-05-31 19:50:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pdf4u/old.git /old
+  git clone https://github.com/Pdf4u/HerokuPDF4U.git /HerokuPDF4U
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /old
+  git clone $UPSTREAM_REPO /HerokuPDF4U
 fi
-cd /old
+cd /HerokuPDF4U
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
