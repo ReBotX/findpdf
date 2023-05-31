c3b6aaddc1bf9e2c6d53a3f1ee79d4c8949926f3
Pdf4u <131749088+Pdf4u@users.noreply.github.com>
2023-05-31 20:02:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['API_ID', '6'])
+API_HASH = environ['API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e']
+BOT_TOKEN = environ['BOT_TOKEN', '1640978391:AAHORFEYfkIG9yJCk7VCyhLlXoMYbiWkZT8']
 
 PORT = environ.get("PORT", "8080")
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
@@ -30,7 +30,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001368430615 -1001609495163').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001368430615')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -38,12 +38,12 @@ FORCE_SUB = environ.get("FORCE_SUB", None)
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://findpdf:findpdf@cluster0.w80nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "pdfmalayalam2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'pdfmalayalam2')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001529292644))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'onlinestorylovers')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
