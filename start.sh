fdfcb3421a61e9774837424bc1e59361f926cce5
ReBotX <newrebot2@outlook.com>
2025-04-06 13:14:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Findpdfbot/FindpdfV2.git /FindpdfV2
+  git clone https://github.com/ReBotX/findpdf.git /findpdf
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FindpdfV2
+  git clone $UPSTREAM_REPO /findpdf
 fi
 cd /HerokuPDF4U
 pip3 install -U -r requirements.txt
